65315d676c744a2cb2d54a3ab16e4c45e3b20046
RafaelaMontanari <rafaela_silva_cunha@hotmail.com>
2020-01-13 17:09:42 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:buscador_de_gifs/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:buscador_de_gifs/ui/home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+        )
+    )
+  ));
+}
+
